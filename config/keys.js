13cf67c0f9bdf5568235ac641676bcf10fbf5386
Jon Kesler <jkesler@hotmail.com>
2020-09-13 13:01:39 -0400

    updated Atlas to DB user

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://jonk:7040@123@cluster0-shard-00-00.a7oy1.mongodb.net:27017,cluster0-shard-00-01.a7oy1.mongodb.net:27017,cluster0-shard-00-02.a7oy1.mongodb.net:27017/jobdash?ssl=true&replicaSet=atlas-6qqglr-shard-0&authSource=admin&retryWrites=true&w=majority",
+    mongoURI: "mongodb://jonkdb:7040123@cluster0-shard-00-00.a7oy1.mongodb.net:27017,cluster0-shard-00-01.a7oy1.mongodb.net:27017,cluster0-shard-00-02.a7oy1.mongodb.net:27017/jobdash?ssl=true&replicaSet=atlas-6qqglr-shard-0&authSource=admin&retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
